0ac1a2a6ecc4429c9cdd0e160ac26abe1954f891
Daniel conteh <contehdaniel1995@gmail.com>
2022-07-02 19:52:53 +0000

    test

--- tour/views.py
+++ tour/views.py
@@ -165,7 +165,7 @@ def CreateCheckoutSessionView(request, tour_slug):
     user_exit = Booked_Tour.objects.filter(Q(user_email=request.user.email) &  Q(tour_slug=tour_slug))
     
     if user_exit.exists():
-         messages.info(request, 'you have booked this tour!!, please book another one!')
+         messages.info(request, 'You have booked this tour!, please book another one!')
          return redirect('/{}'.format(tour_slug))
         
     
